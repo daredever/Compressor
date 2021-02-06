ddff6c043ea4f0269e4d8015750411754b87fc9f
Roman <daredever@gmail.com>
2021-02-06 22:51:29 +0300

    add launch options

---
+++ src/Compressor.Tool/Options.cs
@@ -0,0 +1,23 @@
+﻿using CommandLine;
+
+namespace Compressor.Tool
+{
+    [Verb("compress", HelpText = "Compress file.")]
+    internal sealed class CompressOptions : Options
+    {
+    }
+
+    [Verb("decompress", HelpText = "Decompress file.")]
+    internal sealed class DecompressOptions : Options
+    {
+    }
+
+    internal class Options
+    {
+        [Value(0, MetaName = "input file", HelpText = "Input file to be processed.", Required = true)]
+        public string InputFile { get; set; }
+
+        [Value(1, MetaName = "output file", HelpText = "Output file to be processed.", Required = true)]
+        public string OutputFile { get; set; }
+    }
+}
--- src/Compressor.Tool/Program.cs
+++ src/Compressor.Tool/Program.cs
@@ -1,12 +1,48 @@
 ﻿using System;
+using CommandLine;
 
 namespace Compressor.Tool
 {
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        public static int Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            return Parser.Default
+                .ParseArguments<CompressOptions, DecompressOptions>(args)
+                .MapResult(
+                    (CompressOptions options) => Compress(options),
+                    (DecompressOptions options) => Decompress(options),
+                    errors => 1);
+        }
+
+        private static int Compress(CompressOptions options)
+        {
+            var compressor = new FileCompressor();
+            compressor.Run();
+            return 0;
+        }
+
+        private static int Decompress(DecompressOptions options)
+        {
+            var decompressor = new FileDecompressor();
+            decompressor.Run();
+            return 0;
+        }
+    }
+
+    internal class FileCompressor
+    {
+        public void Run()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class FileDecompressor
+    {
+        public void Run()
+        {
+            throw new NotImplementedException();
         }
     }
 }
